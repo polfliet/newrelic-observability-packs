c1a2f52f6045aa5a91fe0f1e09995abb01e5e997
Stijn Polfliet <spolfliet@newrelic.com>
2021-09-14 14:22:09 +0200

    Add script for updating installPlans and descriptions

---
+++ utils/scripts/mds/java.md
@@ -0,0 +1,11 @@
+**What is <QUICKSTART_NAME>?**
+<QUICKSTART_SUMMARY>
+
+**Get started!**
+[The New Relic Java agent automatically instruments](https://docs.newrelic.com/docs/agents/java-agent/getting-started/compatibility-requirements-java-agent/#auto-instrumented) <QUICKSTART_NAME>. Use this quickstart to quickly get observability for <QUICKSTART_NAME> with New Relic's Java agent. 
+
+**Application Performance Monitoring**
+To view your app's performance in the New Relic UI, go to one.newrelic.com > APM > (select an app) > Summary. The APM user interface includes a dedicated JVM metrics page, transaction and error details, a thread profiler tool to sample Java threads and report stack traces, and more.
+
+**More info**
+Check out the [documentation](https://docs.newrelic.com/docs/agents/java-agent/getting-started/introduction-new-relic-java/) to learn more about New Relic monitoring for <QUICKSTART_NAME>.
---
+++ utils/scripts/updateDescriptions.py
@@ -0,0 +1,112 @@
+import csv
+import pandas
+from ruamel.yaml import YAML
+import os
+from os import path
+import pathlib
+from yaml.loader import SafeLoader
+import sys
+import math
+
+
+# Reading whole csv file with panda library.
+yaml = YAML()
+yaml.width = 4096
+df = pandas.read_csv('quickstarts-descriptions.csv', sep=',')
+
+description_map = {
+    #"<TECHNOLOGY>": ["<SUMMARY>", "DESCRIPTION"],
+    "dotnet": [],
+    "ruby": [],
+    "python": [],
+    "java": [
+        "Monitor <QUICKSTART_NAME> with New Relic's Java agent",
+        "java.md"
+    ],
+    "aws": [],
+    "os": [],
+    "mobile": [],
+    "apache": [],
+    "zookeeper": [],
+    "azure": [],
+    "browser": [],
+    "c": [],
+    "c++": [],
+    "php": [],
+    "cassandra": [],
+    "couchbase": [],
+    "docker": [],
+    "golang": [],
+    "elasticsearch": [],
+    "node.js": [],
+    "f5": [],
+    "gcp": [],
+    "haproxy": [],
+    "jmx": [],
+    "kafka": [],
+    "kubernetes": [],
+    "logstash": [],
+    "mariadb": [],
+    "memcached": [],
+    "micrometer": [],
+    "mongodb": [],
+    "mssql": [],
+    "mysql": [],
+    "nagios": [],
+    "nginx": [],
+    "oracle": [],
+    "windows": [],
+    "postgres": [],
+    "prometheus": [],
+    "rabbitmq": [],
+    "redis": [],
+    "snmp": [],
+    "statsd": [],
+    "synthetics": [],
+    "varnish": [],
+    "esxi": [],
+    "tanzu": []
+}
+
+def dumpToYaml(path, pack_install_key):
+    with open(path, 'r+') as outfile:
+        documents = yaml.load(outfile)
+
+        if pack_install_key in description_map and len(description_map[pack_install_key]) == 2:           
+            summary = description_map[pack_install_key][0]
+            descriptionFile = description_map[pack_install_key][1]
+            # Read .md file
+            file = open("mds/" + descriptionFile)
+            description = file.read()
+            file.close()
+
+            # Replace placeholders
+            summary = summary.replace('<QUICKSTART_NAME>', documents['title'])
+            description = description.replace('<QUICKSTART_NAME>', documents['title'])
+            description = description.replace('<QUICKSTART_SUMMARY>', documents['summary'])
+        
+            documents['summary'] = summary
+            documents['description'] = description      
+            outfile.seek(0) # Move position in outfile to front. 
+            yaml.indent(sequence=4, offset=2)
+            yaml.dump(documents, outfile)
+        else:
+            print(f'{pack_install_key} not defined')
+    outfile.close()
+
+for index, row in df.iterrows(): # Iterates the csv file.
+    print('**** Pack ****')
+    pack_path = str.lower(row['PATH']).replace(' ', '-') # Path of the pack
+    pack_name = row['NAME'] # Name of the pack
+    pack_install_key = row['INSTALL_PLAN'] # Install plan for the pack
+
+    # Find the pack
+    print(f'Path: {pack_path}')
+    path = f'../../packs/{pack_path}/config.yml'
+    
+    if os.path.exists(path): # Check if the pack exists
+        print(f'Pack {pack_name} was found, updating...')
+        dumpToYaml(path, pack_install_key)        
+    else:
+        print(f'Pack {pack_name} was not found at {path}')
+    print('\n')

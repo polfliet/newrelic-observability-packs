11d078800abe70a798d354d870909f104b972d7e
Stijn Polfliet <spolfliet@newrelic.com>
2021-09-20 13:35:55 +0200

    add summary counts

--- utils/scripts/checkQuickstarts.py
+++ utils/scripts/checkQuickstarts.py
@@ -20,21 +20,59 @@ def hasInstallPlan(directory):
 
     return False
 
+def getInstallPlans(directory):
+    #installPlan types: targetedInstall, nerdlet, link
+    with open(directory + "/" + "config.yml") as f:
+        config = yaml.load(f, Loader=SafeLoader)
+        if 'installPlans' in config and len(config['installPlans']) > 0:
+            return config['installPlans']
+
+    return []
+
+def listToString(s): 
+    str = "" 
+    for ele in s: 
+        str += ele + ", "  
+    return str 
+
+countTotal = 0
+countDyesIno = 0
+countDyesIyes = 0
+countDnoIno = 0
+countDnoIyes = 0
+
 def checkDir(directory):
+    global countTotal
+    global countDyesIno
+    global countDyesIyes
+    global countDnoIno
+    global countDnoIyes
     packsRoot = os.listdir(directory)
     for pack in packsRoot:
         if isPack(directory + pack):
+            countTotal += 1
             if hasDashboardOrAlerts(directory + pack):
                 if not hasInstallPlan(directory + pack):
-                    print(pack + ": No installation plan found")
+                    countDyesIno += 1
+                    print(pack + ": DASHB YES INSTALL NO")
                 else:
-                    print(pack + ": Installation plan found")
+                    countDyesIyes += 1                   
+                    print(pack + ": DASHB YES INSTALL YES " + listToString(getInstallPlans(directory + pack)))
             else:
                 if not hasInstallPlan(directory + pack):
-                    print(pack + ": No dashboards or alerts, no installation plan found")
+                    countDnoIno += 1
+                    print(pack + ": DASHB NO INSTALL NO")
                 else:
-                    print(pack + ": No dashboards or alerts, only an installation plan found")
+                    countDnoIyes += 1
+                    print(pack + ": DASHB NO INSTALL YES " + listToString(getInstallPlans(directory + pack)))
         else:
             checkDir(directory + pack + "/")
 
 checkDir("../../packs/")
+
+print("**** RESULTS *****")
+print("Total quickstarts: " + str(countTotal))
+print("Dashboard/alerts YES, Install Plan NO: " + str(countDyesIno))
+print("Dashboard/alerts YES, Install Plan YES: " + str(countDyesIyes))
+print("Dashboard/alerts NO, Install Plan NO: " + str(countDnoIno))
+print("Dashboard/alerts NO, Install Plan YES: " + str(countDnoIyes))
